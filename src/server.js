df8eed0f61ca06acfb6a446b5d2fd6463de3925f
Div-Yas <divyasan1999@gmail.com>
2025-09-17 23:59:59 +0530

    enable fetchnews file in server run

--- src/server.js
+++ src/server.js
@@ -6,5 +6,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
   console.log(`🚀 Server running on port ${PORT}`);
+  require('../scripts/fetchNews');
   // await createCollection(); // ensure Qdrant collection exists
 });
